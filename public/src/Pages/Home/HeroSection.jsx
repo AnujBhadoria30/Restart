79f92f9eb7eb31a5a48c179c360056411b60589c
Anuj Bhadoriya <bhadoriaanuj99@gmail.com>
2024-04-17 15:31:23 +0530

    first commit

--- public/src/Pages/Home/HeroSection.jsx
+++ public/src/Pages/Home/HeroSection.jsx
@@ -1,24 +1,29 @@
-export default function HeroSection() {
+import React from 'react'
+import "./Hero.css"
+
+function HeroSection() {
   return (
     <section id="heroSection" className="hero--section">
       <div className="hero--section--content--box">
         <div className="hero--section--content">
-          <p className="section--title">Hey, I'm John</p>
+          <p className="section--title">Hey, I'm Anuj Bhadoria</p>
           <h1 className="hero--section--title">
             <span className="hero--section-title--color">Full Stack</span>{" "}
             <br />
-            Developer
+            Developer(MERN)
           </h1>
           <p className="hero--section-description">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            <br /> Dolorum, quas. Amet soluta assumenda cum?
+          Aspiring frontend developer eager to contribute fresh perspectives and a strong foundation in web development to create intuitive and dynamic user interfaces
+
           </p>
         </div>
         <button className="btn btn-primary">Get In Touch</button>
       </div>
       <div className="hero--section--img">
-        <img src="./img/hero_img.png" alt="Hero Section" />
+        <div className='myimg'></div>
       </div>
     </section>
-  );
+  )
 }
+
+export default HeroSection
